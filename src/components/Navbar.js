a514f70000e018b632e7bd8a907f53c7536a12e7
samcurteis <samcurteis@gmail.com>
2023-02-17 14:03:05 +0000

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,8 @@
-import { useState, useEffect } from 'react';
 import useViewport from '../hooks/useViewport.js'
 import DesktopNav from './DesktopNav.js'
 import MobileNav from './MobileNav.js'
 
 export default function Navbar() {
-const [dropdown, setDropdown] = useState("70px");
-const [optionDisplay, setOptionDisplay] = useState("none")
 const { width } = useViewport();
 const breakpoint = 500;
 
