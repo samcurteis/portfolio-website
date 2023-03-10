47f60368e80ac7dcf72fbef623de77015a86316c
samcurteis <samcurteis@gmail.com>
2023-03-10 14:02:13 +0000

    fixed error

--- src/lib/notifications.js
+++ src/lib/notifications.js
@@ -1,5 +1,4 @@
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
     
     const SUCCESS = (message) => toast.success(message);
    
