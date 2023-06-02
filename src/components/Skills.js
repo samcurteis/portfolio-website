739702b4f1136c0335a0ed977b06878fd9ecb1eb
samcurteis <samcurteis@gmail.com>
2023-06-02 12:33:13 +0100

    edited skills page to reflect new technologies acquired

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,8 +4,8 @@ import { useInView } from 'react-intersection-observer';
 
 
 export default function Skills() {
-const { ref: skills, inView: skillsInView} = useInView({
-    triggerOnce: true
+    const { ref: skills, inView: skillsInView} = useInView({
+        triggerOnce: true
     });
     return (
         <div  className={"component skills " + (skillsInView ? "show" : "hidden")} ref={skills}>
@@ -20,80 +20,64 @@ const { ref: skills, inView: skillsInView} = useInView({
         <i className="devicon-css3-plain"></i>
         <p className="skill-name">CSS</p>
         </div>  
-  
+
         <div className="skill">  
         <i className="devicon-javascript-plain"></i>     
-             <p className="skill-name">JavaScript</p>
+        <p className="skill-name">JavaScript</p>
         </div>  
         <div className="skill">  
         <i className="devicon-react-original"></i>       
-             <p className="skill-name">React.js</p>
+        <p className="skill-name">React.js</p>
+        </div>  
+        <div className="skill">  
+        <i class="devicon-jest-plain"></i>
+        <p className="skill-name">Jest</p>
         </div>  
         <div className="skill">  
         <i className="devicon-sass-original"></i>        
-             <p className="skill-name">SASS</p>
+        <p className="skill-name">SASS</p>
         </div>  
         <div className="skill">  
         <i className="devicon-express-original"></i>     
-             <p className="skill-name">Express</p>
+        <p className="skill-name">Express</p>
         </div>  
         <div className="skill">  
         <i className="devicon-django-plain"></i>         
-             <p className="skill-name">Django</p>
-        </div>  
-         <div className="skill">  
-        <i className="devicon-flask-original"></i>
-             <p className="skill-name">Flask</p>
+        <p className="skill-name">Django</p>
         </div>  
         <div className="skill">  
         <i className="devicon-python-plain"></i>         
-             <p className="skill-name">Python</p>
+        <p className="skill-name">Python</p>
         </div>  
         <div className="skill">  
         <i className="devicon-java-plain"></i>         
-             <p className="skill-name">Java</p>
+        <p className="skill-name">Java</p>
         </div>  
         <div className="skill">  
         <i className="devicon-spring-plain"></i>         
-             <p className="skill-name">Spring</p>
+        <p className="skill-name">Spring</p>
         </div>  
         <div className="skill">  
         <i className="devicon-mongodb-plain"></i>        
-             <p className="skill-name">MongoDB</p>
+        <p className="skill-name">MongoDB</p>
         </div>  
         <div className="skill">  
         <i className="devicon-postgresql-plain"></i>     
-             <p className="skill-name">PostgreSQL</p>
-        </div>  
-        <div className="skill">  
-        <i class="devicon-docker-plain"></i>
-             <p className="skill-name">Docker</p>
+        <p className="skill-name">PostgreSQL</p>
         </div>  
         <div className="skill">  
         <i className="devicon-git-plain"></i>            
-             <p className="skill-name">Git</p>
+        <p className="skill-name">Git</p>
         </div>  
         <div className="skill">  
         <i className="devicon-heroku-original"></i>      
-             <p className="skill-name">Heroku</p>
-        </div>  
-        <div className="skill">  
-        <i className="devicon-lua-plain"></i>            
-             <p className="skill-name">Lua</p>
+        <p className="skill-name">Heroku</p>
         </div>  
         <div className="skill">  
         <i className="devicon-npm-original-wordmark"></i>
         <p className="skill-name">npm</p>
         </div>         
-        <div className="skill">  
-        <i className="devicon-slack-plain"></i>          
-        <p className="skill-name">Slack</p>
-        </div>         
-        <div className="skill">  
-        <i className="devicon-trello-plain"></i>         
-        <p className="skill-name">Trello</p>
-        </div>         
-</div>
+        </div>
         </div>
     )
 };
