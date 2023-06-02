bd6df5df69fc1b0a058256d5ef46d77c422041bc
samcurteis <samcurteis@gmail.com>
2023-06-02 14:59:02 +0100

    added Realize security to experience. Made cycling photo smaller.

--- src/components/AboutMeComponents/Education.js
+++ src/components/AboutMeComponents/Education.js
@@ -13,20 +13,22 @@ export default function Education() {
         <h3>London</h3>
         <h3>Sep - Dec 2018</h3>
         <h3>City and Islington College</h3>
-<p>Studied teaching English as a second language, student analysis, lesson preparation and delivery with 120+ hours of input lessons and lesson practice.</p>
-
+        <ul>
+        <li>Planned structured lessons that adhered to a string time frame.</li>
+        <li>Presented in front of and supervised the learning of large groups.</li>
+        <li>Delivered close analyses of student's language abilities.</li>
+        </ul>
         </div >
         <div className="piece">
         <h3>BA in English with First Class Honours</h3>
         <h3>London</h3>
         <h3>Sep 2014 - Jun 2017</h3>
- <h3>Queen Mary: University of London</h3>
+        <h3>Queen Mary: University of London</h3>
         <ul>
-<li>Developed analytical skills in reading literature, literary theory, culture, philosophy and history from medieval to contemporary.</li>
-<li>Studied four modules per term, with twelve primary texts, secondary research and four essays per module.</li>
- <li>Conducted research and wrote essays of up to 4000 words on a range of subjects.</li>
-<li>Completed an independent 10,000-word dissertation on theories of performance and narrative in stand-up comedy.</li>
-</ul>
+        <li>Analysed large amounts of text to identify key passages relevant to a given argument.</li>
+        <li>Conducted independent research and produced high-quality essays of up to 10,000 words on a range of subjects.</li>
+        <li>Provided structured evidence using close-reading critical analysis.</li>
+        </ul>
         </div >
         </ div>
     )
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,24 +3,43 @@ export default function Experience() {
    const setting = {
     triggerOnce: true
     }; 
+const { ref: realize, inView: realizeInView} = useInView(setting);
 const { ref: title, inView: titleInView} = useInView(setting);
 const { ref: sei, inView: seiInView} = useInView(setting);
 const { ref: efl, inView: eflInView} = useInView(setting);
-const { ref: lindamood, inView: lindamoodInView} = useInView(setting);
 
     return (
         <div className="component experience">
         <span className="anchor" id="experience"></span>
         <h2 className={"title " + (titleInView ? "show" : "hidden")} ref={title}>Experience</h2>
+        <div className={"experience-section " + (realizeInView ? "show" : "hidden")} ref={realize}>
+        <h3>Junior Developer</h3>
+        <h3 className="location">Remote</h3>
+        <h3>Apr 2023 - Present</h3>
+        <h3>Realize Security</h3>
+        <ul>
+            <li>Contributing to a large codebase in developing both frontend and backend services.</li>
+            <li>Collaborating and pair programming on projects using Git and GitLab.</li>
+            <li>Developing full-stack applications using Test-Driven Development.</li>
+        </ul>
+
+        <p>Technologies: SvelteKit, Django, Vitest, Jest</p>
+
+        </ div>
         <div className={"experience-section " + (seiInView ? "show" : "hidden")} ref={sei}>
         <h3>Software Engineering Immersive</h3>
         <h3 className="location">London (online)</h3>
         <h3>Oct 2022 - Jan 2023</h3>
         <h3>General Assembly</h3>
-        <p>12-week immersive course taken over Zoom and Slack covering the 
-        fundamentals of frontend and backend web development. 
-        It consisted of daily standups, lectures, workshops, pair coding, 
-        daily homework, and four projects.</p>
+        <ul>
+            <li>Built and deployed four projects, two of which are full-stack applications.</li>
+            <li>Utilised agile methodologies to work as part of a team to deliver a product in short sprints.</li>
+            <li>Covered the fundamentals of front-end and back-end engineering, with an introduction to best practice priciples such as SOLID, DRY, OOP and FOP.</li>
+        </ul>
+
+        <p>Technologies: React, SASS, MongoExpress, Node.js, Django, PostgreSQL.</p>
+
+
         </ div>
         <div className={"experience-section " + (eflInView ? "show" : "hidden")} ref={efl}>
         <h3>Teacher of English as a first and second language</h3>
@@ -33,18 +52,8 @@ const { ref: lindamood, inView: lindamoodInView} = useInView(setting);
         <li>Tested and analysed students’ abilities in the areas of speaking, listening, writing, reading, vocabulary and grammar.</li>
         <li>Oversaw long-term study plans to improve students’ abilities.</li>
         </ul>
-        </ div>
-        <div className={"experience-section " + (lindamoodInView ? "show" : "hidden")} ref={lindamood}>
-        <h3>Clinician and Tester</h3>
-        <h3 className="location">London</h3>
-        <h3>Jul 2017 - Dec 2018</h3>
-        <h3>Lindamood-Bell Learning Processes</h3>
-        <ul>
-            <li>Tutored students of all ages and abilities in English and Maths, working mostly with children with learning and behavioural differences.</li>
-            <li>Assessed students' behaviour and abilities in real-time and adjusted teaching approach accordingly.</li>
-            <li>Administered and scored pre and post-course psychometric tests to assess learning progress.</li>
-        </ul>
 
+        <p>Skills: communication, project management, team leadership, critical analysis.</p>
         </ div>
       </div>
     )
