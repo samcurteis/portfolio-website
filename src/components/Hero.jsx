6f96f1345b7b17ad6843200f5d625571c5b065ac
samcurteis <samcurteis@gmail.com>
2026-01-17 11:53:27 +0000

    updated title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,37 @@
 import { useInView } from 'react-intersection-observer';
 
 export default function Hero() {
-const { ref: hero, inView: heroInView} = useInView({
+  const { ref: hero, inView: heroInView } = useInView({
     triggerOnce: true
-    });
+  });
 
-     return (
-        <div className={"component hero " + (heroInView ? "show" : "hidden")} ref={hero}>
-         <div className="content">
+  return (
+    <div
+      className={'component hero ' + (heroInView ? 'show' : 'hidden')}
+      ref={hero}
+    >
+      <div className='content'>
         <h1>Samuel Curteis</h1>
-         <h2>Junior Software Engineer</h2>
-         <div className="icons title">
-       <a href='https://www.linkedin.com/in/samcurteis' target="_blank" rel="noopener noreferrer"> <i className="devicon-linkedin-plain"></i></a>
-       <a href='https://github.com/samcurteis' target="_blank" rel="noopener noreferrer"> <i className="devicon-github-original"></i></a>
-         </div>
-         </div>
-         </div>
-    )
-};
+        <h2>Software Engineer</h2>
+        <div className='icons title'>
+          <a
+            href='https://www.linkedin.com/in/samcurteis'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {' '}
+            <i className='devicon-linkedin-plain'></i>
+          </a>
+          <a
+            href='https://github.com/samcurteis'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {' '}
+            <i className='devicon-github-original'></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
